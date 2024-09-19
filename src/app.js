119db99067b8f1dfcafb3d0b4253058aa96435da
johnSalas11 <salasduranjohn96@gmail.com>
2024-09-19 19:17:46 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["Shrek", "Morty", "Peter griffin", "Marge", "Roger"];
+  let action = ["se tragó", "me robó", "Borró", "se hizo pis en", "pisó"];
+  let what = ["mis proyectos", "mi peluca", "mi motivación", "mi dignidad"];
+
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[randomWho] + " " + action[randomAction] + " " + what[randomWhat];
 };
+
+//git add .
+//git commit -m "generador de excusas"
+//git push
+//copiar link en el apartado de entrega
